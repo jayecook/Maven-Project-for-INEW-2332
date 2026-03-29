493996a1b8cb9255813beb992c5d741c4a49fa15
jayecook <jayecrook@gmail.com>
2026-03-28 20:11:49 -0500

    Create ProductRepository.js
    To be used to communicate directly with the database of information.

---
+++ Backend/src/main/java/com/repository/ProductRepository.js
@@ -0,0 +1,6 @@
+package com.example.productapp.repository;
+
+import com.example.productapp.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {}
